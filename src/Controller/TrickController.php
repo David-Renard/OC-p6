cf66b5f31bff89839aebb0765d6ca9244da4e607
David Renard <color.diff>
2023-10-29 19:36:55 +0100

    updated database, added registration form via maker

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,37 +2,46 @@
 
 namespace App\Controller;
 
-use App\Entity\Trick;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class TrickController extends AbstractController
 {
     #[Route('/', 'homepage')]
-    public function show(EntityManagerInterface $entityManager): Response
+    public function show(TrickRepository $trickRepository): Response
     {
-        $tricks = $entityManager->getRepository(Trick::class)->findBy(array(), ['createdAt' => 'DESC'], 15);
+        $tricks = $trickRepository->findBy(array(), ['createdAt' => 'DESC'], 15);
 
         return $this->render('trick/index.html.twig', ['tricks' => $tricks]);
     }
 
     #[Route('/show_more/{number}', name: 'show_more', requirements: ['number' => '\d+'])]
-    public function showMore(EntityManagerInterface $entityManager, int $number = 0): Response
+    public function showMore(TrickRepository $trickRepository, int $number = 0): Response
     {
-        $tricks = $entityManager->getRepository(Trick::class)->findBy(array(), ['createdAt' => 'DESC'], $number + 15);
+        $tricks = $trickRepository->findBy(array(), ['createdAt' => 'DESC'], $number + 15);
 
         return $this->render('trick/index.html.twig', ['tricks' => $tricks, 'number' => $number]);
     }
 
     #[Route('show/{slug}', name: 'see_trick')]
-    public function showOne(EntityManagerInterface $entityManager, string $slug): Response
+    public function showOne(TrickRepository $trickRepository, string $slug): Response
     {
-        $trick = $entityManager->getRepository(Trick::class)->findOneBy([
+        $trick = $trickRepository->findOneBy([
            'slug' => $slug,
         ]);
 
         return $this->render('trick/show.html.twig', ['trick' => $trick]);
+    }
+
+    #[Route('/new', name: 'new_trick')]
+    #[IsGranted('IS_AUTHENTICATED_REMEMBERED')] // same thing than $this->denyAccessUnlessGranted('ROLE_USER');
+    public function new(): Response
+    {
+//        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        return new Response('bienvenue sur la page de création de tricks');
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,15 +10,24 @@ use App\Entity\User;
 use App\Entity\UserPicture;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use function Symfony\Component\Clock\now;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         // fixture users + userPicture
         $usersAsArray = [["firstname"=>"Océane","name"=>"Barbier","email"=>"oceanebarb@hotmail.com","username"=>"oceba","password"=>"Ocean84-Barb",],["firstname"=>"Milan","name"=>"Julien","email"=>"milanj@yahoo.fr","username"=>"milanju","password"=>"Mila42+Jul",],["firstname"=>"Xavier","name"=>"Dupont","email"=>"xavdu@hotmail.com","username"=>"xadupo","password"=>"Xavier75+Dupon",],["firstname"=>"Lucy","name"=>"Dupuy","email"=>"lucydupuy@yahoo.fr","username"=>"lucdupu","password"=>"Luc156-Dup",],["firstname"=>"Rose","name"=>"Gillet","email"=>"rgil@yahoo.fr","username"=>"roseg","password"=>"Ros88-Gill",],["firstname"=>"Alexis","name"=>"Marie","email"=>"alexmar@yahoo.fr","username"=>"alexmari","password"=>"Alexi77!Mar",],["firstname"=>"Émile","name"=>"Riviere","email"=>"emirivier@yahoo.com","username"=>"emilrivier","password"=>"emi123!Rivier",],["firstname"=>"Florence","name"=>"Bourgeois","email"=>"florencbourgeo@yahoo.com","username"=>"flob","password"=>"Floren7>Bourgeo",],["firstname"=>"Aliyah","name"=>"Poirier","email"=>"apoir@yahoo.fr","username"=>"alipoi","password"=>"Aliya106<Poi",],["firstname"=>"Romy","name"=>"Lemoine","email"=>"romylemo@hotmail.com","username"=>"romle","password"=>"Rom8>Lemoin",],["firstname"=>"Rose","name"=>"Colin","email"=>"roseco@hotmail.fr","username"=>"roco","password"=>"Ros13<Colin",],["firstname"=>"Victor","name"=>"Deschamps","email"=>"victdeschamp@hotmail.com","username"=>"vd","password"=>"Vic19>Deschamp",],["firstname"=>"Hubert","name"=>"Benoit","email"=>"huben@gmail.com","username"=>"hubben","password"=>"Huber43@Benoit",],["firstname"=>"Rosalie","name"=>"Francois","email"=>"rosafranc@yahoo.fr","username"=>"rosaliefranc","password"=>"Rosali84?Fran",],["firstname"=>"Léonie","name"=>"Hubert","email"=>"leonihube@yahoo.fr","username"=>"leonihub","password"=>"Leo6+Hub",],["firstname"=>"Ryan","name"=>"Fleury","email"=>"ryafle@hotmail.fr","username"=>"ryanfleu","password"=>"Rya81!Fleury",],["firstname"=>"Édouard","name"=>"Jacquet","email"=>"edouj@yahoo.com","username"=>"edjacq","password"=>"edouar133!Jacquet",],["firstname"=>"Ryan","name"=>"Michel","email"=>"rmi@hotmail.fr","username"=>"ryamichel","password"=>"Ryan77!Mich",],["firstname"=>"Livia","name"=>"Fleury","email"=>"livfle@gmail.com","username"=>"livifle","password"=>"Livia35?Fleu",],["firstname"=>"Ethan","name"=>"Fournier","email"=>"efournier@gmail.com","username"=>"etf","password"=>"Eth83?Fourni",],["firstname"=>"Charles","name"=>"Royer","email"=>"chroye@hotmail.fr","username"=>"charleroyer","password"=>"Char1@Roy",],["firstname"=>"Lillian","name"=>"Barre","email"=>"libarr@hotmail.com","username"=>"lilliaba","password"=>"Lill137>Bar",],["firstname"=>"David","name"=>"Gerard","email"=>"dagera@yahoo.fr","username"=>"dagerar","password"=>"Davi61@Gerar",],["firstname"=>"Charlotte","name"=>"Durand","email"=>"charloduran@yahoo.com","username"=>"charld","password"=>"Charlo15<Dur",],["firstname"=>"Nicolas","name"=>"Dubois","email"=>"nicoladub@hotmail.com","username"=>"nicduboi","password"=>"Nicola46!Dubois",],["firstname"=>"Julia","name"=>"Gauthier","email"=>"juligauthier@gmail.com","username"=>"jugauthie","password"=>"Julia154@Gauthier",],["firstname"=>"Sarah","name"=>"Dumont","email"=>"sarahd@yahoo.com","username"=>"sarahd","password"=>"Sarah153?Dumont",],["firstname"=>"Annabelle","name"=>"Lefevre","email"=>"annalefe@hotmail.com","username"=>"annlefev","password"=>"Anna107<Lefev",]];
         $users = [];
+        $superAdmin = false;
         foreach ($usersAsArray as $userAsArray) {
             // fixtures unidirectionnal UserPicture
             $avatar = new UserPicture();
@@ -34,8 +43,21 @@ class AppFixtures extends Fixture
             // fixtures User
             $user = new User();
             $user->setEmail($userAsArray['email']);
-            $user->setPassword($userAsArray['password']);
-            $user->setRoles(['ROLE_USER']);
+
+            // set hashed user's password
+            $plainPassword = $userAsArray['password'];
+
+            $user->setPassword($this->passwordHasher->hashPassword($user,$plainPassword));
+            $roles = ['ROLE_USER'];
+            $randomRole = random_int(0,10);
+            if ($randomRole <= 1) {
+                $roles[]='ROLE_ADMIN';
+            }
+            elseif ($randomRole === 0 && !$superAdmin) {
+                $roles[] = 'ROLE_SUPERADMIN';
+                $superAdmin = true;
+            }
+            $user->setRoles($roles);
             $user->setUsername($userAsArray['username']);
             $avatar->setUserInfo($user);
 
